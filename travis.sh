69b23d8594b2fae023d05a2c9c8c35407f21f118
Rahul Somasunderam <rahul.som@gmail.com>
2017-04-23 18:21:52 -0700

    Add sonarqube scanning for master

--- travis.sh
+++ travis.sh
@@ -6,6 +6,7 @@ if [ "$TRAVIS_PULL_REQUEST" = false ]; then
     echo "nexusUsername=$SONATYPE_USER" >> gradle.properties
     echo "nexusPassword=$SONATYPE_PASSWORD" >> gradle.properties
     ./gradlew build uploadArchives --scan --parallel --build-cache --configure-on-demand
+    ./gradlew sonarqube -Dsonar.login=$SONAR_TOKEN -Dsonar.host.url=https://sonarqube.com -Dsonar.organization=rahulsom-github
   else
     ./gradlew check --scan --parallel --build-cache --configure-on-demand
   fi
