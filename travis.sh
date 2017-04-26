7cb0294f5e58d13cfbf2eab48d655959cf5847f9
Rahul Somasunderam <rahul.som@gmail.com>
2017-04-25 18:35:51 -0700

    Prevent dirty flag for travis builds

--- travis.sh
+++ travis.sh
@@ -3,8 +3,8 @@ set -e
 
 if [ "$TRAVIS_PULL_REQUEST" = false ]; then
   if [ "$TRAVIS_BRANCH" = "master" ]; then
-    echo "nexusUsername=$SONATYPE_USER" >> gradle.properties
-    echo "nexusPassword=$SONATYPE_PASSWORD" >> gradle.properties
+    echo "nexusUsername=$SONATYPE_USER" >> ~/.gradle/gradle.properties
+    echo "nexusPassword=$SONATYPE_PASSWORD" >> ~/.gradle/gradle.properties
     ./gradlew build --scan --parallel --build-cache --configure-on-demand
     ./gradlew sonarqube \
             -Dsonar.login=$SONAR_TOKEN \
