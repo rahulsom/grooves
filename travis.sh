ff993568c8c3c08465c8813b12d72b712f7dbb75
Rahul Somasunderam <rahul.som@gmail.com>
2017-05-06 15:07:04 -0700

    Remove parallel build from travis
    When there are a lot of sub projects, travis runs out of memory and
    kills the build

--- travis.sh
+++ travis.sh
@@ -5,15 +5,15 @@ if [ "$TRAVIS_PULL_REQUEST" = false ]; then
   if [ "$TRAVIS_BRANCH" = "master" ]; then
     echo "nexusUsername=$SONATYPE_USER" >> ~/.gradle/gradle.properties
     echo "nexusPassword=$SONATYPE_PASSWORD" >> ~/.gradle/gradle.properties
-    ./gradlew build --scan --parallel --build-cache --configure-on-demand
+    ./gradlew build --scan --build-cache --configure-on-demand
     ./gradlew sonarqube \
             -Dsonar.login=$SONAR_TOKEN \
             -Dsonar.host.url=https://sonarqube.com \
             -Dsonar.organization=rahulsom-github
-    ./gradlew uploadArchives --scan --parallel --build-cache --configure-on-demand
+    ./gradlew uploadArchives --scan --build-cache --configure-on-demand
   else
-    ./gradlew check --scan --parallel --build-cache --configure-on-demand
+    ./gradlew check --scan --build-cache --configure-on-demand
   fi
 else
-  ./gradlew check --scan --parallel --build-cache --configure-on-demand
+  ./gradlew check --scan --build-cache --configure-on-demand
 fi
