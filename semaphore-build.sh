be77b0d770cf305ffeff53eefaa6e3f95a8cc67a
Rahul Somasunderam <rsomasunderam@transcendinsights.com>
2017-06-21 13:57:06 -0700

    Add semaphore build script

---
+++ semaphore-build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function check() {
+    ./gradlew check --scan --build-cache --configure-on-demand
+}
+
+function build() {
+    echo "nexusUsername=$SONATYPE_USER" >> ~/.gradle/gradle.properties
+    echo "nexusPassword=$SONATYPE_PASSWORD" >> ~/.gradle/gradle.properties
+
+    ./gradlew build snapshot --scan --build-cache --configure-on-demand
+
+    ./gradlew sonarqube \
+            -Dsonar.login=$SONAR_TOKEN \
+            -Dsonar.host.url=https://sonarqube.com \
+            -Dsonar.organization=rahulsom-github
+}
+
+if [ "$PULL_REQUEST_NUMBER" != "" ]; then
+    check
+elif [ "$BRANCH_NAME" = "master" ]; then
+    build
+elif [[ "$BRANCH_NAME" =~ ^[0-9]+\.[0-9]+\.x$ ]]; then
+    build
+else
+    check
+fi
