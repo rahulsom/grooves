dec9804f143b05ec6c6d8bea139d03c125188b62
Rahul Somasunderam <rahul.som@gmail.com>
2018-07-14 23:48:00 -0700

    Remove gipsy

--- grooves-diagrams/src/main/groovy/com/github/rahulsom/grooves/asciidoctor/GroovesExtensionRegistry.groovy
+++ grooves-diagrams/src/main/groovy/com/github/rahulsom/grooves/asciidoctor/GroovesExtensionRegistry.groovy
@@ -3,7 +3,6 @@ package com.github.rahulsom.grooves.asciidoctor
 import groovy.transform.CompileStatic
 import org.asciidoctor.Asciidoctor
 import org.asciidoctor.extension.spi.ExtensionRegistry
-import org.kordamp.jipsy.ServiceProviderFor
 
 /**
  * Registers {@link EventsBlock} as <code>esdiag</code>
@@ -11,7 +10,6 @@ import org.kordamp.jipsy.ServiceProviderFor
  * @author Rahul Somasunderam
  */
 @CompileStatic
-@ServiceProviderFor(ExtensionRegistry)
 class GroovesExtensionRegistry implements ExtensionRegistry {
     void register(Asciidoctor asciidoctor) {
         asciidoctor.javaExtensionRegistry().block 'esdiag', EventsBlock
