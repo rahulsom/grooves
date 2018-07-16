057303aa3b1146301f1f65e93eefdb23d3804290
Rahul Somasunderam <rahul.som@gmail.com>
2018-07-15 17:15:25 -0700

    Revert "Remove gipsy"
    This reverts commit dec9804f143b05ec6c6d8bea139d03c125188b62.

--- grooves-diagrams/src/main/groovy/com/github/rahulsom/grooves/asciidoctor/GroovesExtensionRegistry.groovy
+++ grooves-diagrams/src/main/groovy/com/github/rahulsom/grooves/asciidoctor/GroovesExtensionRegistry.groovy
@@ -3,6 +3,7 @@ package com.github.rahulsom.grooves.asciidoctor
 import groovy.transform.CompileStatic
 import org.asciidoctor.Asciidoctor
 import org.asciidoctor.extension.spi.ExtensionRegistry
+import org.kordamp.jipsy.ServiceProviderFor
 
 /**
  * Registers {@link EventsBlock} as <code>esdiag</code>
@@ -10,6 +11,7 @@ import org.asciidoctor.extension.spi.ExtensionRegistry
  * @author Rahul Somasunderam
  */
 @CompileStatic
+@ServiceProviderFor(ExtensionRegistry)
 class GroovesExtensionRegistry implements ExtensionRegistry {
     void register(Asciidoctor asciidoctor) {
         asciidoctor.javaExtensionRegistry().block 'esdiag', EventsBlock
