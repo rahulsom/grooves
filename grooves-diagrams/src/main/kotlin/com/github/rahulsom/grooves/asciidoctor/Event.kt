1462f0520c20939b63e0a9b5bc818f8d1d91e465
Rahul Somasunderam <rahul.som@gmail.com>
2026-01-22 21:06:35 -0800

    refactor: Break up Event.buildSvg for better readability

--- grooves-diagrams/src/main/kotlin/com/github/rahulsom/grooves/asciidoctor/Event.kt
+++ grooves-diagrams/src/main/kotlin/com/github/rahulsom/grooves/asciidoctor/Event.kt
@@ -42,14 +42,32 @@ class Event(
             return G()
         }
 
-        val xOffset = svgBuilder.dates[date]!!
-        // builder.mkp.comment(toString())
         val revertedClass = if (this.reverted) "reverted" else ""
         val g =
             G()
                 .withId("event_$counter")
                 .withClazz("event ${this.type.name} $revertedClass")
 
+        calculatePosition(index, svgBuilder)
+        addRevertLine(g, svgBuilder)
+        if (type == EventType.DeprecatedBy || type == EventType.Join || type == EventType.Disjoin) {
+            val otherId = description.split(' ').last()
+            val other = svgBuilder.allEvents.find { it.id == otherId }!!
+
+            if (other.x > 0 && other.y > 0) {
+                addLinkLine(other, g)
+            }
+        }
+        addCreatedCircle(g)
+        addEventCircleAndText(g, revertedClass)
+        return g
+    }
+
+    private fun calculatePosition(
+        index: Int,
+        svgBuilder: SvgBuilder,
+    ) {
+        val xOffset = svgBuilder.dates[date]!!
         var x = (10 + AGGREGATE_WIDTH * 2 + xOffset * EVENT_SPACE).toInt()
         var y = index * EVENT_LINE_HEIGHT + OFFSET + AGGREGATE_HEIGHT / 2
 
@@ -59,57 +77,69 @@ class Event(
         }
         this.x = x
         this.y = y
+    }
 
+    private fun addRevertLine(
+        g: G,
+        svgBuilder: SvgBuilder,
+    ) {
         if (type == EventType.Revert) {
             val revertedId = description.split(' ').last()
             val reverted = svgBuilder.allEvents.find { it.id == revertedId }!!
 
             val x1 = reverted.x + (this.x - reverted.x) / 2
-            val y1 = y - ((this.x - reverted.x) / 3)
+            val y1 = this.y - ((this.x - reverted.x) / 3)
             g.withSVGDescriptionClassOrSVGAnimationClassOrSVGStructureClass(
                 ObjectFactory().createPath(
-                    ObjectFactory().createPath().withD("M$x $y Q $x1 $y1, ${reverted.x + 15} ${reverted.y - 15}"),
+                    ObjectFactory().createPath().withD(
+                        "M${this.x} ${this.y} Q $x1 $y1, ${reverted.x + 15} ${reverted.y - 15}",
+                    ),
                 ),
             )
         }
+    }
 
-        if (type == EventType.DeprecatedBy || type == EventType.Join || type == EventType.Disjoin) {
-            val otherId = description.split(' ').last()
-            val other = svgBuilder.allEvents.find { it.id == otherId }!!
+    private fun addLinkLine(
+        other: Event,
+        g: G,
+    ) {
+        val x1 = (if (type == EventType.Disjoin) -30 else 30) * abs(other.y - this.y) / EVENT_LINE_HEIGHT
+        val xContactOffset = if (type == EventType.Disjoin) -10 else 10
+        val y1 = (this.y + other.y) / 2
 
-            if (other.x > 0 && other.y > 0) {
-                val x1 = (if (type == EventType.Disjoin) -30 else 30) * abs(other.y - y) / EVENT_LINE_HEIGHT
-                val xContactOffset = if (type == EventType.Disjoin) -10 else 10
-                val y1 = (y + other.y) / 2
-
-                var yOffset = if (y > other.y) 10 else -10
-                if (type == EventType.DeprecatedBy) {
-                    yOffset *= 2
-                }
-
-                g.withSVGDescriptionClassOrSVGAnimationClassOrSVGStructureClass(
-                    ObjectFactory().createPath(
-                        ObjectFactory().createPath().withD(
-                            "M${this.x} $y Q ${x1 + this.x} $y1, ${other.x + xContactOffset} ${other.y + yOffset}",
-                        ),
-                    ),
-                )
-            }
+        var yOffset = if (this.y > other.y) 10 else -10
+        if (type == EventType.DeprecatedBy) {
+            yOffset *= 2
         }
 
+        g.withSVGDescriptionClassOrSVGAnimationClassOrSVGStructureClass(
+            ObjectFactory().createPath(
+                ObjectFactory().createPath().withD(
+                    "M${this.x} ${this.y} Q ${x1 + this.x} $y1, ${other.x + xContactOffset} ${other.y + yOffset}",
+                ),
+            ),
+        )
+    }
+
+    private fun addCreatedCircle(g: G) {
         if (description.contains("created")) {
             g.withSVGDescriptionClassOrSVGAnimationClassOrSVGStructureClass(
                 ObjectFactory().createCircle(
                     Circle()
                         .withCx("${this.x}")
-                        .withCy("$y")
+                        .withCy("${this.y}")
                         .withR("14")
                         .withStrokeDasharray("2, 5")
                         .withClazz("eventCreated ${this.type.name}"),
                 ),
             )
         }
+    }
 
+    private fun addEventCircleAndText(
+        g: G,
+        revertedClass: String,
+    ) {
         g.withSVGDescriptionClassOrSVGAnimationClassOrSVGStructureClass(
             ObjectFactory().createCircle(
                 ObjectFactory()
@@ -130,6 +160,5 @@ class Event(
                     .withContent(this.id),
             ),
         )
-        return g
     }
 }
